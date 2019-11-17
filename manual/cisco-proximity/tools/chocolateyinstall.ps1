459d949e43810c13d42ca13206206b6e2fdd37cc
Brice McIver <github@bricemciver.com>
2019-11-17 00:25:36 -0600

    Add installer option to auto-accept EULA

--- manual/cisco-proximity/tools/chocolateyinstall.ps1
+++ manual/cisco-proximity/tools/chocolateyinstall.ps1
@@ -13,7 +13,7 @@ $packageArgs = @{
   checksum      = 'eb05d112b6ab99039dcc3baccb5080ae77f3279beb869be4f7c0c40724f575c7'
   checksumType  = 'sha256'
 
-  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`" ACCEPTEULA=1"
   validExitCodes= @(0, 3010, 1641)
 }
 
